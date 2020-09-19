1168e5c59e39423fce9a19c361d9f38504dcb3fc
abrower10612 <abrower7@byui.edu>
2020-09-18 22:26:33 -0600

    asdf

---
+++ js/portal.js
@@ -0,0 +1,8 @@
+var test = document.lastModified;
+
+var d = new Date();
+var n = d.getFullYear();
+
+document.getElementById("currentYear").innerHTML = n;
+
+document.getElementById("lastUpdated").innerHTML = test;
