6ea8e8cc0abb8d027efae51cc8a41a6d3913a970
abrower10612 <abrower7@byui.edu>
2020-10-17 17:15:37 -0600

    adsf

--- lesson05/js/scripts.js
+++ lesson05/js/scripts.js
@@ -51,7 +51,7 @@ const today = new Date();
 const dayNumber = today.getDay();
 const element = document.getElementById('message');
 
-if (dayNumber == 6) {
+if (dayNumber == 5) {
   element.classList.add("showme")
 }
 
