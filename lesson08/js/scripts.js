0508f758f7f9e6b63c8e3f509628a4d8f777cce4
abrower10612 <abrower7@byui.edu>
2020-11-07 21:33:38 -0700

    asdf

--- lesson08/js/scripts.js
+++ lesson08/js/scripts.js
@@ -59,3 +59,17 @@ else {
   element.classList.add("hideme")
 }
 
+// <----------------FORM SLIDER------------------------------>
+
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+const s = document.querySelector('#selected')
+const sel = document.querySelector('#selectbrowser');
+s.style.display = "block";
+s.textContent = sel.value;
+
+}
+
