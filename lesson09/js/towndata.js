23185eef67c274f9c317136e60ca7de42bd307f8
abrower10612 <abrower7@byui.edu>
2020-11-14 23:31:34 -0700

    asdf

--- lesson09/js/towndata.js
+++ lesson09/js/towndata.js
@@ -6,16 +6,18 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const towndata = jsonObject['towns'];
-    let towns = towndata.filter(town => (town.name == 'Fish Haven'));
+    let towns = towndata.filter(town => (town.name == 'Fish Haven' || town.name == 'Preston' || town.name == 'Soda Springs'));
     towns.forEach(town => {
       let card = document.createElement('section');
       let name = document.createElement('h2');
+      let motto = document.createElement('motto')
       let founded = document.createElement('h3');
       let population = document.createElement('h4')
       let rainfall = document.createElement('h4')
       let img = document.createElement('img');
 
       name.innerHTML = `${town.name}`;
+      motto.innerHTML = `${town.motto}`;
       founded.innerHTML = `Year Founded: ${town.yearFounded}`;
       population.innerHTML = `Population: ${town.currentPopulation}`;
       rainfall.innerHTML = `Average Rainfall: ${town.averageRainfall}`;
@@ -23,82 +25,15 @@ fetch(requestURL)
       img.setAttribute('alt', town.name);
 
       card.appendChild(name);
+      card.appendChild(motto);
       card.appendChild(founded);
       card.appendChild(population);
       card.appendChild(rainfall);
       card.appendChild(img);
 
-      document.querySelector('.fishHaven').appendChild(card);
+      document.querySelector('.towns').appendChild(card);
     })
     
 
   });
 
-  fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    const towndata = jsonObject['towns'];
-    let towns = towndata.filter(town => (town.name == 'Preston'));
-    towns.forEach(town => {
-      let card = document.createElement('section');
-      let name = document.createElement('h2');
-      let founded = document.createElement('h3');
-      let population = document.createElement('h4')
-      let rainfall = document.createElement('h4')
-      let img = document.createElement('img');
-
-      name.innerHTML = `${town.name}`;
-      founded.innerHTML = `Year Founded: ${town.yearFounded}`;
-      population.innerHTML = `Population: ${town.currentPopulation}`;
-      rainfall.innerHTML = `Average Rainfall: ${town.averageRainfall}`;
-      img.setAttribute('src', `images/${town.photo}`);
-      img.setAttribute('alt', town.name);
-
-      card.appendChild(name);
-      card.appendChild(founded);
-      card.appendChild(population);
-      card.appendChild(rainfall);
-      card.appendChild(img);
-
-      document.querySelector('.preston').appendChild(card);
-    })
-
-    fetch(requestURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (jsonObject) {
-      const towndata = jsonObject['towns'];
-      let towns = towndata.filter(town => (town.name == 'Soda Springs'));
-      towns.forEach(town => {
-        let card = document.createElement('section');
-        let name = document.createElement('h2');
-        let founded = document.createElement('h3');
-        let population = document.createElement('h4')
-        let rainfall = document.createElement('h4')
-        let img = document.createElement('img');
-  
-        name.innerHTML = `${town.name}`;
-        founded.innerHTML = `Year Founded: ${town.yearFounded}`;
-        population.innerHTML = `Population: ${town.currentPopulation}`;
-        rainfall.innerHTML = `Average Rainfall: ${town.averageRainfall}`;
-        img.setAttribute('src', `images/${town.photo}`);
-        img.setAttribute('alt', town.name);
-  
-        card.appendChild(name);
-        card.appendChild(founded);
-        card.appendChild(population);
-        card.appendChild(rainfall);
-        card.appendChild(img);
-  
-        document.querySelector('.sodaSprings').appendChild(card);
-      })
-  
-      
-  
-    });
-    
-
-  });
