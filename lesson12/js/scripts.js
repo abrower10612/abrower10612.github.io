902e7a102105c64bf1daa56c3ba867c61dd02daa
abrower10612 <abrower7@byui.edu>
2020-12-04 22:49:54 -0700

    asdf

--- lesson12/js/scripts.js
+++ lesson12/js/scripts.js
@@ -1,5 +1,13 @@
-// <----------------TOGGLE MENU------------------------>
+// <----------------TOGGLE MENU, HAM, AND X------------------------>
 
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide"); 
+}
+
+function toggleHam() {
+  document.getElementById("hamID").classList.toggle("hide"); 
+}
+
+function toggleX() {
+  document.getElementById("xID").classList.toggle("hide"); 
 }
