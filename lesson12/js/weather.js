fa8f5ab17b3d23ba24edbfbdc5b91f7e447d99a4
abrower10612 <abrower7@byui.edu>
2020-12-06 00:05:27 -0700

    asdf

--- lesson12/js/weather.js
+++ lesson12/js/weather.js
@@ -54,7 +54,7 @@ fetch(apiURL)
         forecastDayNumber = 0;
       }
 
-      let theDayName = document.createElement('span');
+      let theDayName = document.createElement('h3');
       theDayName.textContent = weekday[forecastDayNumber];
 
       console.log(theDayName);
