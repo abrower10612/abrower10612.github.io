2d8e2634d804074e5747b23760f81ffa38894b86
abrower10612 <abrower7@byui.edu>
2020-12-16 23:46:27 -0700

    asdf

--- lesson12/js/prices.js
+++ lesson12/js/prices.js
@@ -32,7 +32,60 @@ fetch(pricesURL)
 
     document.getElementById('dioFDW').innerHTML = priceInfo.Dio.WalkIn.FullDay;
 
+    // PCX150
 
+    document.getElementById('PCX150').innerHTML = priceInfo.PCX150.Rental;
 
+    document.getElementById('PCX150Max').innerHTML = priceInfo.PCX150.MaxPersons;
+
+    document.getElementById('PCX150HDR').innerHTML = priceInfo.PCX150.Reservation.HalfDay;
+
+    document.getElementById('PCX150FDR').innerHTML = priceInfo.PCX150.Reservation.FullDay;
+
+    document.getElementById('PCX150HDW').innerHTML = priceInfo.PCX150.WalkIn.HalfDay;
+
+    document.getElementById('PCX150FDW').innerHTML = priceInfo.PCX150.WalkIn.FullDay;
+
+    // PIONEER
+
+    document.getElementById('Pioneer').innerHTML = priceInfo.Pioneer.Rental;
+
+    document.getElementById('PioneerMax').innerHTML = priceInfo.Pioneer.MaxPersons;
+
+    document.getElementById('PioneerHDR').innerHTML = priceInfo.Pioneer.Reservation.HalfDay;
+
+    document.getElementById('PioneerFDR').innerHTML = priceInfo.Pioneer.Reservation.FullDay;
+
+    document.getElementById('PioneerHDW').innerHTML = priceInfo.Pioneer.WalkIn.HalfDay;
+
+    document.getElementById('PioneerFDW').innerHTML = priceInfo.Pioneer.WalkIn.FullDay;
+
+    // FOURDOOR
+
+    document.getElementById('FourDoor').innerHTML = priceInfo.FourDoor.Rental;
+
+    document.getElementById('FourDoorMax').innerHTML = priceInfo.FourDoor.MaxPersons;
+
+    document.getElementById('FourDoorHDR').innerHTML = priceInfo.FourDoor.Reservation.HalfDay;
+
+    document.getElementById('FourDoorFDR').innerHTML = priceInfo.FourDoor.Reservation.FullDay;
+
+    document.getElementById('FourDoorHDW').innerHTML = priceInfo.FourDoor.WalkIn.HalfDay;
+
+    document.getElementById('FourDoorFDW').innerHTML = priceInfo.FourDoor.WalkIn.FullDay;
+
+    // TWODOOR
+
+    document.getElementById('TwoDoor').innerHTML = priceInfo.TwoDoor.Rental;
+
+    document.getElementById('TwoDoorMax').innerHTML = priceInfo.TwoDoor.MaxPersons;
+
+    document.getElementById('TwoDoorHDR').innerHTML = priceInfo.TwoDoor.Reservation.HalfDay;
+
+    document.getElementById('TwoDoorFDR').innerHTML = priceInfo.TwoDoor.Reservation.FullDay;
+
+    document.getElementById('TwoDoorHDW').innerHTML = priceInfo.TwoDoor.WalkIn.HalfDay;
+
+    document.getElementById('TwoDoorFDW').innerHTML = priceInfo.TwoDoor.WalkIn.FullDay;
 
   });
