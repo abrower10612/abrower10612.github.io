9cebb0bae732e0c989657bbacdb74b93559aff4e
abrower10612 <abrower7@byui.edu>
2020-12-16 22:42:27 -0700

    asdf

--- lesson12/js/prices.js
+++ lesson12/js/prices.js
@@ -4,6 +4,12 @@ fetch(pricesURL)
   .then((response) => response.json())
   .then((priceInfo) => {
 
-    console.log(priceInfo["HondaMetroScooter"]["MaxPersons"])
+    console.log(priceInfo.JeepWrangler4Door.MaxPersons)
+
+    let metroMax = priceInfo.HondaMetroScooter.MaxPersons
+
+    console.log(metroMax)
+
+    document.getElementById('rentType').innerHTML = priceInfo.HondaMetroScooter.MaxPersons;
 
   });
