da5a96a6eec1019a1bfb16d68a26a7ae737cc88d
abrower10612 <abrower7@byui.edu>
2020-12-17 01:13:01 -0700

    asdf

---
+++ lesson12/js/lastUpdate.js
@@ -0,0 +1,8 @@
+var d = new Date();
+var n = d.getFullYear();
+
+var test = document.lastModified;
+
+document.getElementById("currentYear").innerHTML = n;
+
+document.getElementById("lastUpdated").innerHTML = test;
